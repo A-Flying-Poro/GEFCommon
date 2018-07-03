d9beec599de9afbdc554259a4f9d56824ddf3632
Oskar Stangenberg <Crokoking@googlemail.com>
2018-07-03 20:36:43 +0200

    Set up link support in the Game-construct logic and wired up all dependant modules. Added map-key-type for MapLinks to annotation-processing.

--- src/main/java/com/universeprojects/gefcommon/shared/elements/GameAspect.java
+++ src/main/java/com/universeprojects/gefcommon/shared/elements/GameAspect.java
@@ -1,6 +1,8 @@
 package com.universeprojects.gefcommon.shared.elements;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public interface GameAspect<K> {
     // Standard field access..
@@ -13,4 +15,16 @@ public interface GameAspect<K> {
     GameObject<K> getGameObject();
 
     Collection<String> getPropertyNames();
+
+    void setSubObjectSingle(String name, GameObject<K> object);
+
+    void setSubObjectList(String name, List<GameObject<K>> object);
+
+    void setSubObjectMap(String name, Map<?, GameObject<K>> object);
+
+    Map<String, GameObject<K>> getSubObjectsSingle();
+
+    Map<String, List<GameObject<K>>> getSubObjectsList();
+
+    Map<String, Map<?, GameObject<K>>> getSubObjectsMap();
 }
