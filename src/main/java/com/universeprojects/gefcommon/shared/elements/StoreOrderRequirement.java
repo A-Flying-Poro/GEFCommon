16e62676869f9f390666e8fdf2218f2a016f223a
Aranem <ryan.kwandary@gmail.com>
2018-07-11 12:31:31 -0400

    Created command to get all store orders for a specific character and CommandStoreOrderListHandler. Created datastructure to store a store order from the database. Added associated test command to ClientAdminCommandHandler.

---
+++ src/main/java/com/universeprojects/gefcommon/shared/elements/StoreOrderRequirement.java
@@ -0,0 +1,11 @@
+package com.universeprojects.gefcommon.shared.elements;
+
+import java.util.List;
+
+public interface StoreOrderRequirement {
+     String getName ();
+
+     String getKey ();
+
+     List getFieldFilters ();
+}
